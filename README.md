713dd33b190c4a6469afdde336a66b8ee010afbf
Gergely Nagy <algernon@madhouse-project.org>
2016-12-09 11:08:30 +0100

    Initial import
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ README.md
@@ -0,0 +1,18 @@
+# Borealis-inspired color scheme
+
+This is a [Borealis][borealis]-inspired color scheme for the [Keyboardio Model
+01][m01], a plugin for [KeyboardioFirmware][kbdio:firmware].
+
+See my [blog post][asylum:borealis] about the story behind the plugin, and for
+documentation.
+
+ [m01]: https://shop.keyboard.io/
+ [kbdio:firmware]: https://github.com/Keyboardio/KeyboardioFirmware
+ [borealis]: http://pimpmykeyboard.com/dsa-borealis-keyset/
+ [asylum:borealis]: https://asylum.madhouse-project.org/blog/2016/12/09/Akela/#borealis
+
+License
+=======
+
+The code is released under the terms of the GNU GPL, version 3 or later. See the
+COPYING file for details.
---
+++ src/Borealis.cpp
@@ -0,0 +1,53 @@
+/* -*- mode: c++ -*-
+ * Keyboardio-Borealis -- Borealis-inspired color scheme
+ * Copyright (C) 2016  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Borealis.h"
+
+BorealisEffect::BorealisEffect (void) {
+  LEDControl.mode_add (this);
+}
+
+void
+BorealisEffect::update (void) {
+  static cRGB colors[] = {
+    {0x67, 0x2d, 0x5f},
+    {0x7e, 0x4b, 0x8e},
+    {0x22, 0x5b, 0x90},
+    {0x35, 0x86, 0xb5},
+    {0x40, 0xad, 0xce},
+    {0x8b, 0xfa, 0xe7},
+  };
+
+  // Update all the LEDs, each row with its own color.
+  for (byte row = 0; row < ROWS; row++) {
+    for (byte col = 0; col < COLS; col++) {
+      led_set_crgb_at (row, col, colors[row]);
+    }
+  }
+
+  // Override the thumb arcs, they should be treated as their own row.
+  for (byte row = 0; row < 4; row++) {
+    for (byte col = 7; col < 9; col++) {
+      led_set_crgb_at (row, col, colors[4]);
+    }
+  }
+
+  // Override the palm keys too, they are the final row.
+  led_set_crgb_at (3, 6, colors[5]);
+  led_set_crgb_at (3, 9, colors[5]);
+}
