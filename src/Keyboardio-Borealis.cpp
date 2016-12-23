15804662eaa9a46028fa9a7cfe0040a7b1967386
Gergely Nagy <algernon@madhouse-project.org>
2016-12-23 15:19:51 +0100

    Rename from Borealis.* to Keyboardio-Borealis.*
    To follow the Arduino library guidelines, and to make it easier to use.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/Keyboardio-Borealis.cpp
+++ src/Keyboardio-Borealis.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Borealis.h"
+#include "Keyboardio-Borealis.h"
 
 BorealisEffect::BorealisEffect (void) {
   LEDControl.mode_add (this);
